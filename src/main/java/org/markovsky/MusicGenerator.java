cffe80431633a02ea5e6c623bf63e261297e9610
Sam Baumgarten <sambaumgarten98@gmail.com>
2016-04-04 09:37:02 -0700

    Add music generator

---
+++ src/main/java/org/markovsky/MusicGenerator.java
@@ -0,0 +1,24 @@
+package org.markovsky;
+
+import java.util.LinkedList;
+
+/**
+ * Created by sambaumgarten on 4/4/16
+ */
+public class MusicGenerator {
+    public static Song generateMusic(Note startNote, TransitionMatrix<Note> transitionMatrix) {
+        LinkedList<Note> notes = new LinkedList<>();
+
+        Note currentNote = startNote;
+        while (currentNote != null) {
+            notes.add(currentNote);
+            if (transitionMatrix.probabilities(currentNote) != null) {
+                currentNote = transitionMatrix.probabilities(currentNote).randomNode();
+            } else {
+                currentNote = null;
+            }
+        }
+
+        return new Song((Note [])notes.toArray(new Note[notes.size()]));
+    }
+}
---
+++ src/main/java/tests/MusicGeneratorTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.markovsky.MusicGenerator;
+import org.markovsky.Note;
+import org.markovsky.Song;
+import org.markovsky.TransitionMatrix;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by sambaumgarten on 4/4/16
+ */
+public class MusicGeneratorTest {
+    @Test
+    public void testGenerateMusic() throws Exception {
+        TransitionMatrix<Note> noteTransitionMatrix = new TransitionMatrix<>();
+
+        Note note1 = new Note(1.0, 30);
+        Note note2 = new Note(1.0, 40);
+
+        noteTransitionMatrix.recordTransition(note1, note2);
+
+        Song song = MusicGenerator.generateMusic(note1, noteTransitionMatrix);
+
+        assertEquals(song, new Song(new Note[] {note1, note2}));
+    }
+}
